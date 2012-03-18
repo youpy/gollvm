d45c7efebced7f54f4e214aae6ce64b161309ba4
krasin <imkrasin@gmail.com>
2012-03-18 00:57:20 -0700

    Update installation script and instructions to use 'go get'

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export CGO_CFLAGS="`llvm-config --cflags`"
 export CGO_LDFLAGS="`llvm-config --ldflags` -Wl,-L`llvm-config --libdir` -lLLVM-`llvm-config --version`"
-go install $* github.com/axw/gollvm/llvm
+go get $* github.com/axw/gollvm/llvm
