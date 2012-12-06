54fe12db281f52ec4aad665de4fe06d7ef4ed081
Andrew Wilkins <axwalk@gmail.com>
2012-12-06 23:29:58 +0800

    Add LLVM 3.2 headers (llvm-c and minimal subset of llvm)
    I've added these headers so that gollvm/llgo will build with
    the Native Client SDK (PNaCl).

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-export CGO_CFLAGS="`llvm-config --cflags`"
+export CGO_CFLAGS="`llvm-config --cflags` -I ../include"
 export CGO_LDFLAGS="`llvm-config --ldflags` -Wl,-L`llvm-config --libdir` -lLLVM-`llvm-config --version`"
 go get $* github.com/axw/gollvm/llvm
